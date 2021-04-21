11d3d21b5c69d274df878c58f518d4161cb32e6d
Rob Perrin <robert.perrin@ucalgary.ca>
2021-04-21 11:07:49 -0600

    added plot seismic locs

--- build/lib/octopus/rpfcn.py
+++ build/lib/octopus/rpfcn.py
@@ -3,14 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-def plot_seis(filename):
+def plot_seis(filename,aspect_ratio):
     fseis = segyio.open(filename, ignore_geometry=True)
 
     seis_data = fseis.trace.raw[:]
 
     fseis.close()
 
-    plt.imshow(seis_data.T,cmap=plt.cm.bone,aspect=5)
+    plt.imshow(seis_data.T,cmap=plt.cm.bone,aspect=aspect_ratio)
     plt.colorbar()
     plt.grid()
     plt.show()
@@ -21,3 +21,15 @@ def print_ebcdic(filename):
     print(fseis.text)
 
     fseis.close()
+
+def plot_seis_locs(filename,mult):
+
+    groupX = fseis.attributes(segyio.TraceField.GroupX)[:]*mult
+    groupY = fseis.attributes(segyio.TraceField.GroupY)[:]*mult
+
+    plt.figure(figsize=(10,10))
+    plt.plot(groupX,groupY)
+    plt.grid()
+    plt.xlabel('Easting (m)')
+    plt.ylabel('Northing (m)')
+    plt.show()
